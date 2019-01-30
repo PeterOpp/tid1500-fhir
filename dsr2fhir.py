ec143311a695aa316c014881b057cd1d926012a9
Hans Meine <hans_meine@gmx.net>
2019-01-30 09:52:43 +0000

    fix identifier value key

--- dsr2fhir.py
+++ dsr2fhir.py
@@ -28,7 +28,7 @@ def patient_resource(root):
         family = _tag_value(root, 'PatientName')
     )
     result['identifier'] = dict(
-        family = _tag_value(root, 'PatientID')
+        value = _tag_value(root, 'PatientID')
     )
     result['gender'] = DICOM_SEX_TO_FHIR_GENDER[_tag_value(root, 'PatientSex')]
 
